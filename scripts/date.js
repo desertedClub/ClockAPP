2d9678222a51342ba64452ad0d4833f0395f1a8b
desertedClub <kordelia777@gmail.com>
2025-03-03 01:44:45 +0200

    Fixed date, working correctly.

--- scripts/date.js
+++ scripts/date.js
@@ -1,21 +1,20 @@
 document.addEventListener("DOMContentLoaded", function() {
-    setInterval(showDate, 1000)
-    showDate()
+    function updateDate() {
+        let date = new Date()
+        let day = date.getDate()
+        let monthNumber = date.getMonth()
+        let year = date.getFullYear()
+
+        let months = ["January", "February", "March", "April", "May", "June",
+            "July", "August", "September", "October", "November", "December"]
+
+        let monthName = months[monthNumber]
+
+        document.getElementById("day").textContent = day
+        document.getElementById("month").textContent = monthName
+        document.getElementById("year").textContent = year
+        }
+
+        updateDate()
+        setInterval(updateDate, 1000)
 })
-
-var months = ["January", "February", "March", "April", "May", "June",
-                "July", "August", "September", "October", "November", "December"
-]
-
-function showDate() {
-    let date = new Date()
-    let day = date.getDate()
-    let monthNumber = date.getMonth()
-    let year = date.getFullYear()
-
-    let monthName = months[monthNumber]
-
-    document.getElementById("day").textContent = day
-    document.getElementById("month").textContent = monthName
-    document.getElementById("year").textContent = year
-}
